66057c09088763ffcc440b6469a1c98545265efa
DANCHUSKY <danielmorenodoncel8@gmail.com>
2023-12-05 19:22:12 +0100

    [FEAT] clase Cajero

--- src/mercado/Mercado.java
+++ src/mercado/Mercado.java
@@ -4,6 +4,8 @@
  */
 package mercado;
 
+import java.util.Scanner;
+
 /**
  *
  * @author daniel
@@ -16,6 +18,19 @@ public class Mercado {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("primer commit");
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese un número
+        System.out.print("Ingrese un número: ");
+
+        // Leer el número ingresado por el usuario
+        int numero = scanner.nextInt();
+
+        System.out.println(numero);
+
+        // Cerrar el objeto Scanner después de su uso
+        scanner.close();
     }
     
 }
